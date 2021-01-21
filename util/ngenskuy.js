792032d1a3dcd16d6009175c8f8af9d9918e641c
NgenSal <77656749+NgenSal@users.noreply.github.com>
2021-01-22 06:11:59 +0700

    Create ngenskuy.js

---
+++ util/ngenskuy.js
@@ -0,0 +1,10 @@
+let config;
+
+try {
+  config = require("../config.json");
+} catch (error) {
+  config = null;
+}
+
+exports.TOKEN = config ? config.TOKEN : process.env.TOKEN;
+exports.PREFIX = config ? config.PREFIX : process.env.PREFIX;
